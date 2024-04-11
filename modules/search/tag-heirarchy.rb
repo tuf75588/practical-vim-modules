bd8856d33137568537ac30b9cbc77bf5bebf072c
Andrew Davis <atd285@gmail.com>
2024-04-11 16:08:51 -0400

    search practice

--- modules/search/tag-heirarchy.rb
+++ modules/search/tag-heirarchy.rb
@@ -6,5 +6,5 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/dnvim2 for more book information.
 #---
-class XhtmlDocument < XmlDocument; end
-class XhtmlTag < XmlTag; end
+class jsonDocument < jsonDocument; end
+class jsonTag < jsonTag; end
